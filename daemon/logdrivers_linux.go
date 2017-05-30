93cf5011b9e777080b194e20855f3251506fc583
Miles Chen <miles.chen01@sap.com>
2017-05-30 16:10:46 +0800

    Enable all drivers

--- daemon/logdrivers_linux.go
+++ daemon/logdrivers_linux.go
@@ -3,9 +3,9 @@ package daemon
 import (
 	// Importing packages here only to make sure their init gets called and
 	// therefore they register themselves to the logdriver factory.
-	_ "github.com/docker/docker/daemon/logger/jsonfilelog"
-	// _ "github.com/docker/docker/daemon/logger/kafkalog"
-	_ "github.com/docker/docker/daemon/logger/loghub"
 	_ "github.com/docker/docker/daemon/logger/fifo"
-	// _ "github.com/docker/docker/daemon/logger/redislog"
+	_ "github.com/docker/docker/daemon/logger/jsonfilelog"
+	_ "github.com/docker/docker/daemon/logger/kafkalog"
+	_ "github.com/docker/docker/daemon/logger/loghub"
+	_ "github.com/docker/docker/daemon/logger/redislog"
 )
